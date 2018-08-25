ef9c8a564e0f0f5dd721cc3cbfeff3a5187a567f
phvalue <ph_value@naver.com>
2018-08-26 03:33:55 +0900

    Changed the Html for the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):                                                         # 함수 생성
+    return render(request, 'blog/post_list.html', {})
